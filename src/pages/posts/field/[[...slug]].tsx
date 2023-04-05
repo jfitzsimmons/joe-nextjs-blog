782fb7c4b49e0395122d3d93beeaba3a5e0c2238
jfitzsimmons <jfitzcode@gmail.com>
2023-04-05 16:26:37 -0500

    no lint errors, todo testjpf fixes

--- src/pages/posts/field/[[...slug]].tsx
+++ src/pages/posts/field/[[...slug]].tsx
@@ -1,69 +1,83 @@
-import { GetStaticPaths, GetStaticProps } from "next";
-import Layout from "../../../features/components/layout/Layout";
-import BasicMeta from "../../../common/components/meta/BasicMeta";
-import OpenGraphMeta from "../../../common/components/meta/OpenGraphMeta";
-import TwitterCardMeta from "../../../common/components/meta/TwitterCardMeta";
-import FieldList from "../../../features/components/lists/FieldList";
-import config from "../../../common/utils/config";
-import { PostContent, Field } from "../../../lib/posts";
-import { listPostRefs, countRefs } from "../../../lib/references";
-import { getField, listFields, FieldContent } from "../../../lib/fields";
-import { TagContent } from "../../../common/utils/tags";
+import React from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Layout from '../../../features/components/layout/Layout'
+import BasicMeta from '../../../common/components/meta/BasicMeta'
+import OpenGraphMeta from '../../../common/components/meta/OpenGraphMeta'
+import TwitterCardMeta from '../../../common/components/meta/TwitterCardMeta'
+import FieldList from '../../../features/components/lists/FieldList'
+import config from '../../../common/utils/config'
+import { PostContent, Field } from '../../../lib/posts'
+import { listPostRefs, countRefs } from '../../../lib/references'
+import { getField, listFields, FieldContent } from '../../../lib/fields'
+// import { TagContent } from '../../../common/types'
 
 type Props = {
-  posts: Field[];
-  field: FieldContent;
-  tags: TagContent[];
-  page?: string;
+  posts: Field[]
+  field: FieldContent
+  // tags: TagContent[];
+  page?: string
   pagination: {
-    current: number;
-    pages: number;
-  };
-};
-export default function Index({ posts, field, tags, pagination, page }: Props) {
-  const url = `/posts/field/${field.name}` + (page ? `/${page}` : "");
-  const title = field.name;
+    current: number
+    pages: number
+  }
+}
+export default function Index({ posts, field, pagination, page }: Props) {
+  const url = `/posts/field/${field.name}${page ? `/${page}` : ''}`
+  const title = field.name
   return (
     <Layout>
-      <BasicMeta url={url} title={title} />
-      <OpenGraphMeta url={url} title={title} />
-      <TwitterCardMeta url={url} title={title} />
-      <FieldList fields={posts} pagination={pagination} field={field}/>
+      <BasicMeta
+        url={url}
+        title={title}
+      />
+      <OpenGraphMeta
+        url={url}
+        title={title}
+      />
+      <TwitterCardMeta
+        url={url}
+        title={title}
+      />
+      <FieldList
+        fields={posts}
+        pagination={pagination}
+        field={field}
+      />
     </Layout>
-  );
+  )
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const queries = params.slug as string[];
-  const [slug, page] = [queries[0], queries[1]];
+  const queries = params.slug as string[]
+  const [slug, page] = [queries[0], queries[1]]
   const posts = listPostRefs(
-    page ? parseInt(page as string) : 1,
+    page ? parseInt(page as string, 10) : 1,
     config.refs_per_page,
-  );
-  const field = getField(slug);
-  //const tags = childTags(field.name);
+  )
+  const field = getField(slug)
+  // const tags = childTags(field.name);
   const pagination = {
-    current: page ? parseInt(page as string) : 1,
+    current: page ? parseInt(page as string, 10) : 1,
     pages: Math.ceil(posts.length / config.refs_per_page),
-  };
+  }
   const props: {
-    posts: PostContent[];
-    field: FieldContent;
-    //tags: TagContent[];
-    pagination: { current: number; pages: number };
-    page?: string;
-  } = { posts, field, /*tags,*/ pagination };
+    posts: PostContent[]
+    field: FieldContent
+    // tags: TagContent[];
+    pagination: { current: number; pages: number }
+    page?: string
+  } = { posts, field, /* tags, */ pagination }
   if (page) {
-    props.page = page;
+    props.page = page
   }
   return {
     props,
-  };
-};
+  }
+}
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = listFields().flatMap((field) => {
-    const pages = Math.ceil(countRefs() / config.refs_per_page);
+    const pages = Math.ceil(countRefs() / config.refs_per_page)
     return Array.from(Array(pages).keys()).map((page) =>
       page === 0
         ? {
@@ -71,11 +85,15 @@ export const getStaticPaths: GetStaticPaths = async () => {
           }
         : {
             params: { slug: [field.slug, (page + 1).toString()] },
-          }
-    );
-  });
+          },
+    )
+  })
   return {
-    paths: paths,
+    paths,
     fallback: false,
-  };
-};
+  }
+}
+
+Index.defaultProps = {
+  page: 'Insincere Engineer',
+}
--- src/pages/posts/field/categories/[[...slug]].tsx
+++ src/pages/posts/field/categories/[[...slug]].tsx
@@ -1,72 +1,96 @@
-import { GetStaticPaths, GetStaticProps } from "next";
-import Layout from "../../../../features/components/layout/Layout";
-import BasicMeta from "../../../../common/components/meta/BasicMeta";
-import OpenGraphMeta from "../../../../common/components/meta/OpenGraphMeta";
-import TwitterCardMeta from "../../../../common/components/meta/TwitterCardMeta";
-import FieldList from "../../../../features/components/lists/FieldList";
-import config from "../../../../common/utils/config";
-import { PostContent, Field } from "../../../../lib/posts";
-import { listPostRefs, countRefs } from "../../../../lib/references";
-import { getField, FieldContent } from "../../../../lib/fields";
-import { FilterContent, listCats, getCat } from "../../../../common/utils/categories";
+import React from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Layout from '../../../../features/components/layout/Layout'
+import BasicMeta from '../../../../common/components/meta/BasicMeta'
+import OpenGraphMeta from '../../../../common/components/meta/OpenGraphMeta'
+import TwitterCardMeta from '../../../../common/components/meta/TwitterCardMeta'
+import FieldList from '../../../../features/components/lists/FieldList'
+import config from '../../../../common/utils/config'
+import { PostContent, Field } from '../../../../lib/posts'
+import { listPostRefs, countRefs } from '../../../../lib/references'
+import { getField, FieldContent } from '../../../../lib/fields'
+import { listCats, getCat } from '../../../../common/utils/categories'
+import { FilterContent } from '../../../../common/types'
 
 type Props = {
-  posts: Field[];
-  field: FieldContent;
-  category: FilterContent;
-  page?: string;
+  posts: Field[]
+  field: FieldContent
+  category: FilterContent
+  page?: string
   pagination: {
-    current: number;
-    pages: number;
-  };
-};
+    current: number
+    pages: number
+  }
+}
 
-export default function Index({ posts, field, category, pagination, page }: Props) {
-  const url = `/posts/field/${category.name}` + (page ? `/${page}` : "");
-  const title = category.name;
+export default function Index({
+  posts,
+  field,
+  category,
+  pagination,
+  page,
+}: Props) {
+  const url = `/posts/field/${category.name}${page ? `/${page}` : ''}`
+  const title = category.name
   return (
     <Layout>
-      <BasicMeta url={url} title={title} />
-      <OpenGraphMeta url={url} title={title} />
-      <TwitterCardMeta url={url} title={title} />
-      <FieldList fields={posts} pagination={pagination} field={field} tag={category}/>
+      <BasicMeta
+        url={url}
+        title={title}
+      />
+      <OpenGraphMeta
+        url={url}
+        title={title}
+      />
+      <TwitterCardMeta
+        url={url}
+        title={title}
+      />
+      <FieldList
+        fields={posts}
+        pagination={pagination}
+        field={field}
+        tag={category}
+      />
     </Layout>
-  );
+  )
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const queries = params.slug as string[];
-  const [slug, page] = [queries[0], queries[1]];
+  const queries = params.slug as string[]
+  const [slug, page] = [queries[0], queries[1]]
   const posts = listPostRefs(
-    page ? parseInt(page as string) : 1,
+    page ? parseInt(page as string, 10) : 1,
     config.refs_per_page,
     slug,
-    'category'
-  );
-  const field = getField('references');
-  const category = getCat(slug);
+    'category',
+  )
+  const field = getField('references')
+  const category = getCat(slug)
   const pagination = {
-    current: page ? parseInt(page as string) : 1,
+    current: page ? parseInt(page as string, 10) : 1,
     pages: Math.ceil(posts.length / config.refs_per_page),
-  };
+  }
   const props: {
-    posts: PostContent[];
-    field: FieldContent;
-    category: FilterContent;
-    pagination: { current: number; pages: number };
-    page?: string;
-  } = { posts, field, /*tags,*/ category, pagination };
+    posts: PostContent[]
+    field: FieldContent
+    category: FilterContent
+    pagination: { current: number; pages: number }
+    page?: string
+  } = { posts, field, /* tags, */ category, pagination }
   if (page) {
-    props.page = page;
+    props.page = page
   }
   return {
     props,
-  };
-};
+  }
+}
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = listCats().flatMap((category) => {
-    const pages = Math.ceil(countRefs(category.slug,'category') / config.refs_per_page);
+    const pages = Math.ceil(
+      countRefs(category.slug, 'category') / config.refs_per_page,
+    )
     return Array.from(Array(pages).keys()).map((page) =>
       page === 0
         ? {
@@ -74,11 +98,15 @@ export const getStaticPaths: GetStaticPaths = async () => {
           }
         : {
             params: { slug: [category.slug, (page + 1).toString()] },
-          }
-    );
-  });
+          },
+    )
+  })
   return {
-    paths: paths,
+    paths,
     fallback: false,
-  };
-};
+  }
+}
+
+Index.defaultProps = {
+  page: 'Insincere Engineer',
+}
