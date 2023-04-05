04fe1489c838671b45e407faf20f9610d4f14202
jfitzsimmons <jfitzcode@gmail.com>
2023-04-05 18:11:10 -0500

    list modules and linting

--- src/features/components/lists/CategoryButton.tsx
+++ src/features/components/lists/CategoryButton.tsx
@@ -1,35 +1,19 @@
-import Link from "next/link";
-import { FilterContent } from "../../common/utils/categories";
-
+import React from 'react'
+import Link from 'next/link'
+import { FilterContent } from '../../../common/types'
+import styles from './CategoryButton.module.css'
+// background-color: ${(cat.color) ? `rgba(${  cat.color  }.8)` : 'rgba(21, 132, 125, 0.2)'};
+// testjpf style thing above again
 type Props = {
-  cat: FilterContent;
-};
+  cat: FilterContent
+}
 export default function CategoryButton({ cat }: Props) {
   return (
-    <>
-      <Link href={"/posts/categories/[[...slug]]"} as={`/posts/categories/${cat.slug}`}>
-        <a>{cat.name}</a>
-      </Link>
-      <style jsx>{`
-        a {
-          display: inline-block;
-          border-radius: 0 4vmin 4vmin 0 ;
-          background-color: ${(cat.color) ? 'rgba(' + cat.color + '.8)' : 'rgba(21, 132, 125, 0.2)'};
-          color: #211;
-          transition: background-color 0.3s ease;
-          padding: 0.25em 0.5em;
-          writing-mode: vertical-rl;
-          transform: rotate(180deg);
-          text-align: center;
-          text-shadow: 0px 0px 2px #fee8
-        }
-        a:active,
-        a:hover {
-          background-color: #feec; 
-          text-shadow: 0 0 2px #1008;
-          color: #fff;
-        }
-      `}</style>
-    </>
-  );
+    <Link
+      href="/posts/categories/[[...slug]]"
+      as={`/posts/categories/${cat.slug}`}
+    >
+      <span className={styles.span}>cat.name</span>
+    </Link>
+  )
 }
