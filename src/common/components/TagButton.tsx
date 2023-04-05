f1476d8de511d0d08a6981ba5d32a1748fc3988a
jfitzsimmons <jfitzcode@gmail.com>
2023-04-05 17:44:36 -0500

    further css modules, further lint

--- src/common/components/TagButton.tsx
+++ src/common/components/TagButton.tsx
@@ -1,23 +1,23 @@
-import Link from "next/link";
-import { TagContent } from "../utils/tags";
+import React from 'react'
+import Link from 'next/link'
+import { TagContent } from '../types'
+import styles from './TagButton.module.css'
 
 type Props = {
-  tag: TagContent;
-};
+  tag: TagContent
+}
 export default function TagButton({ tag }: Props) {
   return (
-    <>
-      <Link href={"/posts/tags/[[...slug]]"} as={`/posts/tags/${tag.slug}`}>
-        <a className="tag">#{tag.name}</a>
-      </Link>
-      <style jsx>
-        {`
-          .tag {
-            text-shadow: 0 0 1px #000;
-            font-family: Cambria, "Hoefler Text", Utopia, "Liberation Serif", "Nimbus Roman No9 L Regular", Times, "Times New Roman", serif;
-          }
-        `}
-      </style>
-    </>
-  );
+    <Link
+      href="/posts/tags/[[...slug]]"
+      as={`/posts/tags/${tag.slug}`}
+    >
+      <a
+        className={styles.tag}
+        href="/posts/tags/[[...slug]]"
+      >
+        #{tag.name}
+      </a>
+    </Link>
+  )
 }
